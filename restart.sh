047f3565b55e42fc69c85ca9b4e63d1a21934105
rinh <hao.lin@qunar.com>
2013-06-24 19:08:11 +0800

    修改重启脚本

--- restart.sh
+++ restart.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+path=$(cd $(dirname $0);echo $PWD)
 
-[[ -e ./app.nohup ]] && kill -QUIT `cat ./app.nohup`
-sleep 10
-nohup sudo npm start >/dev/null 2>&1 &
-echo $! > ./app.nohup
+[[ -e $path/pid.nohup ]] && kill -QUIT `cat $path/pid.nohup`
+sleep 2
+cd $path
+nohup sudo node $path/node_modules/.bin/coffee $path/src/cli.coffee >/dev/null 2>&1 &
+echo $! > $path/pid.nohup
