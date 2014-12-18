a4f0b26b36ad7e247e7e7698065cad2e9a84e19f
rinh <hao.lin@qunar.com>
2014-12-18 12:11:48 +0800

    update shell

--- restart.sh
+++ restart.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-path=$(cd $(dirname $0);echo $PWD)
 
-[[ -e $path/pid.nohup ]] && kill -QUIT `cat $path/pid.nohup`
-sleep 2
+#path=$(cd $(dirname $0);echo $PWD)
+#[[ -e $path/pid.nohup ]] && kill -QUIT `cat $path/pid.nohup`
+#sleep 2
+#cd $path
+#nohup sudo node $path/node_modules/.bin/coffee $path/src/cli.coffee >/dev/null 2>&1 &
+#echo $! > $path/pid.nohup
 cd $path
-nohup sudo node $path/node_modules/.bin/coffee $path/src/cli.coffee >/dev/null 2>&1 &
-echo $! > $path/pid.nohup
+sudo node $path/node_modules/.bin/coffee -c -o lib src
+sudo pm2 restart fekit-package-server
